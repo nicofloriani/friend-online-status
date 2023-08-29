93f896f36c4350aaf5d1b141eeba0335f34e825d
nico_floriani <floriani.nico@gmail.com>
2023-08-29 09:53:31 -0700

    Created state, useEffect function with async try / catch function, extraction of JSON, passed data to Friend component, and rendered to screen

--- src/Friend.js
+++ src/Friend.js
@@ -1,11 +1,14 @@
-const Friend = () => {
+const Friend = ({ friend }) => {
+  const letterAvatar = friend.name.split("")[0].toUpperCase();
   return (
     <div className="friend-card">
       <div className="friend-details">
-        <div className="avatar"></div>
-        <h2 className="friend-name"></h2>
+        <div className="avatar">{letterAvatar}</div>
+        <h2 className="friend-name">{friend.name}</h2>
       </div>
-      <div className="friend-status"></div>
+      <div
+        className={`friend-status ${friend.online ? "online" : "offline"}`}
+      ></div>
     </div>
   );
 };
